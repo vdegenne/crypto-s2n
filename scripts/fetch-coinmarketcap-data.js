8c40c581e695ad395b637cd7fda7e51bb4b921a7
vdegenne <vdegenne@gmail.com>
2024-08-10 13:20:12 +0200

    2024-08-10

--- scripts/fetch-coinmarketcap-data.js
+++ scripts/fetch-coinmarketcap-data.js
@@ -13,7 +13,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 	let info = await res.json();
 	info.data.cryptoCurrencyMap.map((i) => {
 		// map[i.symbol] = i.slug;
-		map[i.symbol] = i.name;
+		if (!map[i.symbol]) {
+			map[i.symbol] = i.name;
+		}
 	});
 	res = await fetch(
 		'https://api.coinmarketcap.com/data-api/v3/map/all?listing_status=active,untracked&exchangeAux=is_active,status&cryptoAux=is_active,status&start=10001&limit=10000',
@@ -24,7 +26,9 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 	// );
 	info.data.cryptoCurrencyMap.map((i) => {
 		// map[i.symbol] = i.slug;
-		map[i.symbol] = i.name;
+		if (!map[i.symbol]) {
+			map[i.symbol] = i.name;
+		}
 	});
 
 	fs.writeFileSync(`${__dirname}/../sn-map.json`, JSON.stringify(map));
